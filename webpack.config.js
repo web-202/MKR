713c550aea9387e00780b049fd015bae912d60bc
Denis <77487610+Denis11333@users.noreply.github.com>
2023-11-11 11:43:41 +0200

    init

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,3 @@
-// webpack.config.js
 const path = require('path');
 const TerserPlugin = require('terser-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
@@ -16,6 +15,68 @@ module.exports = (env, argv) => {
             extensions: ['.ts', '.js'],
         },
         module: {
+            rules: [
+                // TypeScript
+                {
+                    test: /\.ts$/,
+                    use: 'ts-loader',
+                    exclude: /node_modules/,
+                },
+                // JavaScript/ES6+ through Babel
+                {
+                    test: /\.js$/,
+                    exclude: /node_modules/,
+                    use: {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['@babel/preset-env'],
+                        },
+                    },
+                },
+                // HTML
+                {
+                    test: /\.html$/,
+                    use: 'html-loader',
+                },
+                // Images
+                {
+                    test: /\.(png|jpg|jpeg|gif|svg)$/,
+                    use: [
+                        {
+                            loader: 'url-loader',
+                            options: {
+                                limit: 8192,
+                                name: '[name].[ext]',
+                                outputPath: 'images',
+                            },
+                        },
+                        {
+                            loader: 'image-webpack-loader',
+                            options: {
+                                mozjpeg: {
+                                    progressive: true,
+                                    quality: 65,
+                                },
+                                optipng: {
+                                    enabled: false,
+                                },
+                                pngquant: {
+                                    quality: [0.65, 0.90],
+                                    speed: 4,
+                                },
+                                gifsicle: {
+                                    interlaced: false,
+                                },
+                            },
+                        },
+                    ],
+                },
+                // SASS/SCSS
+                {
+                    test: /\.(scss|sass)$/,
+                    use: ['style-loader', 'css-loader', 'sass-loader'],
+                },
+            ],
         },
         plugins: [
             new HtmlWebpackPlugin({
