c4afe99fbbb42b02cf28f45e8afd3744326eeda8
Denis <77487610+Denis11333@users.noreply.github.com>
2023-11-11 11:30:30 +0200

    init

---
+++ app/index.ts
@@ -0,0 +1,6 @@
+// index.ts
+const appElement = document.getElementById('app');
+
+if (appElement) {
+  appElement.innerHTML = '<h1>Hello, TypeScript!</h1>';
+}
---
+++ webpack.config.js
@@ -0,0 +1,61 @@
+const path = require('path');
+const TerserPlugin = require('terser-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+
+module.exports = (env, argv) => {
+    const isProduction = argv.mode === 'production';
+
+    const plugins = [
+        new HtmlWebpackPlugin({
+            template: './app/index.html',
+            filename: 'index.html',
+        }),
+    ];
+
+    if (isProduction) {
+        plugins.push(
+          new HtmlWebpackPlugin({
+              template: './app/index.html',
+              filename: 'index.html',
+              minify: {
+                  collapseWhitespace: true,
+                  removeComments: true,
+                  removeRedundantAttributes: true,
+                  removeScriptTypeAttributes: true,
+                  removeStyleLinkTypeAttributes: true,
+                  useShortDoctype: true,
+              },
+          }),
+          new webpack.DefinePlugin({
+              'process.env.NODE_ENV': JSON.stringify('production'),
+          })
+        );
+    }
+
+    return {
+        entry: './app/index.ts',
+        output: {
+            filename: 'bundle.js',
+            path: path.resolve(__dirname, 'dist'),
+        },
+        module: {
+            rules: [
+            ],
+        },
+        resolve: {
+            extensions: ['.ts', '.js'],
+        },
+        plugins: plugins,
+        optimization: {
+            minimizer: [
+                new TerserPlugin({
+                    extractComments: false,
+                }),
+            ],
+        },
+        devServer: {
+            port: 0,
+        },
+    };
+};
